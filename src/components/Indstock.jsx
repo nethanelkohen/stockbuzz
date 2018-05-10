601a85ba44b14ef40524c59b2a2f1c67d5acf704
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-05-09 22:27:15 -0400

    second graph

--- src/components/Indstock.jsx
+++ src/components/Indstock.jsx
@@ -11,7 +11,8 @@ class Stocks extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      indStock: {}
+      changePercent: {},
+      volumeData: {}
     };
   }
 
@@ -19,43 +20,51 @@ class Stocks extends Component {
     let stocks = this.props.data;
     let match = this.props.match;
     let entries = Object.entries(stocks);
-    // console.log(entries);
     entries.map(key => {
       if (match.params.id === key[0]) {
-        // let slice = key[1].chart.slice(-7);
-        let array = [];
+        let volumeArray = [];
+        let percentArray = [];
         let newKey = key[1].chart.slice(-7);
         for (let i = 0; i < newKey.length; i++) {
-          // let vol = `${(newKey[i].volume / 1000).toFixed(1)}M`;
-          let vol = newKey[i].volume;
-          let date = newKey[i].label;
-
-          let volData = {
+          const percentChange = newKey[i].changePercent;
+          const vol = newKey[i].volume;
+          const date = newKey[i].label;
+          const volData = {
             [date]: vol
           };
-          array.push(volData);
-          let obj = Object.assign({}, ...array);
-          this.setState({ indStock: obj });
+          const percentData = {
+            [date]: percentChange
+          };
+          volumeArray.push(volData);
+          let volumeObj = Object.assign({}, ...volumeArray);
+          percentArray.push(percentData);
+          let percentObj = Object.assign({}, ...percentArray);
+          console.log(percentData);
+          this.setState({
+            volumeData: volumeObj,
+            changePercent: percentObj
+          });
         }
       }
     });
+    this.renderData();
   }
+
+  renderData = volData => {};
 
   render() {
     let stocks = this.props.data;
     let match = this.props.match;
-    let graphData = this.state.indStock;
-    console.log('graphData', graphData);
-    // function kFormatter(num) {
-    //   return num > 9999 ? (num / 10000).toFixed(1) + 'M' : num;
-    // }
-    // kFormatter(graphData.vol);
+    let graphData = this.state.volumeData;
+    let percentData = this.state.changePercent;
+    console.log(this.state.changePercent);
+
     return (
       <div>
         {Object.entries(stocks).map((key, index) => {
           if (match.params.id === key[0]) {
             return (
-              <div>
+              <div key={index}>
                 <h1>{match.params.id}</h1>
                 <h1>{key[1].quote.close}</h1>
               </div>
@@ -69,8 +78,9 @@ class Stocks extends Component {
           data={graphData}
           messages={{ empty: 'No data' }}
         />
+        <BarChart data={percentData} messages={{ empty: 'No data' }} />
         <p>Chart only displays business days</p>
-        {/* {this.state.indStock ? <LineChart data={graphData} /> : null} */}
+        {/* {this.state.volData ? <LineChart data={graphData} /> : null} */}
       </div>
     );
   }
